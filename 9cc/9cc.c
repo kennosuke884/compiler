a4fb967e9345949c6d96b4165a9e2f723120727e
kennosuke884 <kennosuke.basketball.884@gmail.com>
2022-08-18 19:13:53 +0900

    コメントを追加

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//how to compile : gcc hoge.c -o hoge
+//how to run : ./hoge
+
 // int argc, char **argv はコマンドライン引数
 // コマンドライン引数とは、コンピュータのコマンド入力画面（コマンドライン）からプログラムを起動する際に指定する文字列
 // argc:引数の個数　argv:引数の配列　**argv:引数配列のポインタ
