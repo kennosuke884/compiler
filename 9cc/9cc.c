e056b51a4e6bb9222cce580ca46ae177ed69c481
kennosuke884 <kennosuke.basketball.884@gmail.com>
2022-08-18 19:09:21 +0900

    コメント追加

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// int argc, char **argv はコマンドライン引数
+// コマンドライン引数とは、コンピュータのコマンド入力画面（コマンドライン）からプログラムを起動する際に指定する文字列
+// argc:引数の個数　argv:引数の配列　**argv:引数配列のポインタ
 int main(int argc, char **argv){
     if(argc != 2){
         fprintf(stderr, "引数の個数が正しくありません\n");
