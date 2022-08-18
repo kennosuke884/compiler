0fb7df9f364a89907901854fb6732bebe542678a
kennosuke884 <kennosuke.basketball.884@gmail.com>
2022-08-18 19:38:29 +0900

    足し算と引き算を追加

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -12,10 +12,32 @@ int main(int argc, char **argv){
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));  //atoi():整数値のstr型データをint型に変換する関数
-    printf("  ret\n");
+    printf("  mov rax, %ld\n", strtol(p, &p, 10)); //strtol():文字列を整数型の数値に変換する関数 文字列pを10を基数にして変換。
+    //変換が成功したら、変換終了位置へのアドレスが&pへ格納
+    
+    while(*p){
+        if(*p == '+'){
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if(*p == '-'){
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
+    printf("    ret\n");
     return 0;
 }
